9488fed56d06d490d414bd061220485ce80fff8b
Oskar J <Olgittmar@gmail.com>
2021-05-19 17:05:50 +0200

    Trying to figure out how to setup conan to use ninja with cmake while keeping build, source and install separate

---
+++ Cplusplus/CPlusPlusConfig.h
@@ -0,0 +1,3 @@
+#define CPlusPlus_VERSION_MAJOR 0
+#define CPlusPlus_VERSION_MINOR 1
+#define CPlusPlus_VERSION_PATCH 0
---
+++ Cplusplus/main.cpp
@@ -0,0 +1,9 @@
+#include <ostream>
+using namespace std;
+
+int
+main( int argc, char* argv[] )
+{
+    printf( "Hello World!" );
+    return 0;
+}
